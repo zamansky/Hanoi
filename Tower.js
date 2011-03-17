bb02ab80c399008710d187e6bb2609fb351d758b
Mike Zamansky <zamansky@gmail.com>
2011-03-17 12:32:54 -0400

    Started messing with draw in Tower.h to learn about the canvas -- hanoi.js is now entry point for the program

--- Tower.js
+++ Tower.js
@@ -1,11 +1,17 @@
-function Tower(x,y,gc) {
-    this.gc = gc;
+function Tower(x,y,height,width,canvas) {
+    this.canvas=canvas;
+    this.gc = canvas.getContext("2d");
     this.x = x;
     this.y = y;
+    this.height=height;
+    this.width=width;
     this.discs = new Array();
 }
 
-Tower.prototype.draw {
-
-
+Tower.prototype.draw = function()  {
+    this.gc.fillStyle="rgb(255,0,0)";
+    this.gc.fillRect(this.x,this.y+this.canvas.height-30,100,10);
+    this.gc.strokeStyle="rgb(0,0,0)";
+    this.gc.strokeRect(this.x,this.y+this.canvas.height-30,100,10);
+    console.log(this.canvas.height);
 }
