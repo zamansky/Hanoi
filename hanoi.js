78d89d0843122d9b3478a011e45333510f44af64
Mike Zamansky <zamansky@gmail.com>
2011-03-19 15:45:28 -0400

    basic ui done

--- hanoi.js
+++ hanoi.js
@@ -2,29 +2,22 @@
 
 
 
-// Towers is global for debugging purposes
-var Towers = new Array();
 
 // Globals so that they can be changed via the UI
+var Towers;
 var numTowers = 3;
-var numColors = 2;
+var numColors = 1;
 var numDisks = 3;
 var canvas; // load the canvas in start (after page is loaded)
 var height = 400;
 var width = 800;
 
-/*------------------------- start --------------------------*/
+/*------------------------- makeTowersandDisks --------------------------*/
 /*
- * application starting point
+ * 
  */
-
-
-function start() {
-    
-    // We can't set canvas until after the page is loaded
-    canvas = document.getElementById("canvas");
-
-    // Make the towers
+function makeTowersandDisks(){
+    Towers = new Array();
     for (var i=0;i<numTowers;i++) {
 	var tBlockWidth= width/numTowers;
 	var tCenterX   = i*tBlockWidth+tBlockWidth/2;
@@ -40,18 +33,40 @@ function start() {
     // Make the starting disks
     for (var i=numDisks;i>0;i--)
 	Towers[0].addDisk(new Disk(canvas,0,0,i*25+25,i,numColors));
-    
-    // Draw to start
+}
+
+/*------------------------- drawTowers --------------------------*/
+/*
+ * 
+ */
+function drawTowers() {
     for (t in Towers){
 	Towers[t].draw(canvas.getContext("2d"));
     }
+}
 
+
+/*------------------------- start --------------------------*/
+/*
+ * application starting point
+ */
+function start() {
+    
+    // We can't set canvas until after the page is loaded
+    canvas = document.getElementById("canvas");
+
+    makeTowersandDisks();
+    drawTowers();
+
+
+    bindButtons();
 
     /*
      * make an array of moves and run them.
      * This is for testing purposes only.
      * remove after ui is done.
      */
+	/*
     moveList = new Array();
     moveList.push(new Move(0,1));
     moveList.push(new Move(0,2));
@@ -72,6 +87,7 @@ function start() {
     // play the above move list
     moveListIntervalID=setInterval(playMoves,1,canvas,Towers);
 
+	 */
 
 }
 
