ad2de0af936d8575b9a11785b2afc017ff351036
Mike Zamansky <zamansky@gmail.com>
2011-03-19 14:50:54 -0400

    Cleaned up Disk.js

--- Disk.js
+++ Disk.js
@@ -1,3 +1,10 @@
+/*------------------------- Disk --------------------------*/
+
+
+/*------------------------- Disk Constructor --------------------------*/
+/*
+ * Disks can only be 1 or 2 color
+ */
 function Disk(canvas,x,y,size,number,colors){
     this.canvas=canvas;
     this.gc = canvas.getContext("2d");
@@ -12,19 +19,23 @@ function Disk(canvas,x,y,size,number,colors){
     this.bColor=("rgb(255,0,0)");
     if (this.colors==2)
 	this.bColor=("rgb(0,0,255)");
-
-    this.orientation=1;
 }
 
 
+/*------------------------- draw --------------------------*/
+/*
+ * x,y is the center of the disk
+ */
 
 Disk.prototype.draw = function () {
     this.gc.fillStyle=this.tColor;
-    this.gc.fillRect(this.x-this.width/2,this.y-this.height/2,this.width,this.height/2);
+    this.gc.fillRect(this.x-this.width/2,this.y-this.height/2,
+		     this.width,this.height/2);
     this.gc.fillStyle=this.bColor;
     this.gc.fillRect(this.x-this.width/2,this.y,this.width,this.height/2);
     this.gc.strokeStyle="rgb(0,0,0)";
-    this.gc.strokeRect(this.x-this.width/2,this.y-this.height/2,this.width,this.height);
+    this.gc.strokeRect(this.x-this.width/2,this.y-this.height/2,
+		       this.width,this.height);
 }
 
 
